8c4e5344c4a0e133e5e415f7c98d4f6699267c53
sakib <sakiburrahman11.msr@gmail.com>
2024-05-24 23:24:32 +0600

    banner control

--- resources/views/frontend/master_dashboard.blade.php
+++ resources/views/frontend/master_dashboard.blade.php
@@ -13,7 +13,6 @@
     <meta name="keywords" content="{{ $seo->meta_keyword }}" />
     <meta name="description" content="{{ $seo->meta_description }}" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta property="og:title" content="" />
     <meta property="og:type" content="" />
