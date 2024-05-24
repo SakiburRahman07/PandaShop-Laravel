f337bb0424d59f1400a9891e56ad9de7b32525ff
sakib <sakiburrahman11.msr@gmail.com>
2024-05-24 23:30:22 +0600

    seo control

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,7 +24,6 @@
 
 
 
-
 <div class="col-md-9">
 <div class="tab-content account dashboard-content pl-50">
 <div class="tab-pane fade active show" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
