2b18fbdec9cbd40fe3342f1b185304981d5a8f11
sakib <sakiburrahman11.msr@gmail.com>
2024-05-24 23:15:30 +0600

    slider control

--- resources/views/frontend/master_dashboard.blade.php
+++ resources/views/frontend/master_dashboard.blade.php
@@ -12,7 +12,6 @@
     <meta name="author" content="{{ $seo->meta_author }}" />
     <meta name="keywords" content="{{ $seo->meta_keyword }}" />
     <meta name="description" content="{{ $seo->meta_description }}" />
-
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <meta name="viewport" content="width=device-width, initial-scale=1" />
