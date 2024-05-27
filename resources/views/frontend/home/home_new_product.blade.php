ff2f36b1bd32f8f351be0dbedbb13dd3864c7419
sakib <sakiburrahman11.msr@gmail.com>
2024-05-27 23:46:00 +0600

    Inactive vendor's product don't show

--- resources/views/frontend/home/home_new_product.blade.php
+++ resources/views/frontend/home/home_new_product.blade.php
@@ -24,6 +24,10 @@
                         <div class="row product-grid-4">
 
     @foreach($products as $product)
+    @php
+    $ven_status = App\Models\User::where('id', $product->vendor_id)->value('status');
+    @endphp
+    @if($ven_status == 'active')
     <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
         <div class="product-cart-wrap mb-30 wow animate__animated animate__fadeIn" data-wow-delay=".1s">
             <div class="product-img-action-wrap">
@@ -130,6 +134,7 @@ $avarage = App\Models\Review::where('product_id',$product->id)->where('status',1
             </div>
         </div>
     </div> 
+    @endif
     <!--end product card-->
     @endforeach
 
